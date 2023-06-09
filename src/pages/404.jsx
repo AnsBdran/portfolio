e0e8fe4ca1b4fae791ef3dc5a7e8309218624122
Anas Badran <ansaccts@gmail.com>
2023-06-09 17:58:19 +0300

    added 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
+import { Section } from '../components/elements';
+import Footer from '../components/sections/Footer';
+import Header from '../components/sections/Header';
+
 const Error404 = () => {
-  return <div>Error404</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Section>
+          <section className='flex-1'>
+            <h1>Error 404</h1>
+            <p>The page you are looking for is not found.</p>
+            <div className='mt-3'>
+              <Link to='/'>
+                <Button>Return home</Button>
+              </Link>
+            </div>
+          </section>
+          <div className='flex-1'>
+            <img src='assets/404.png' alt='404 error illustration' />
+          </div>
+        </Section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Error404;
--- src/router.jsx
+++ src/router.jsx
@@ -8,10 +8,11 @@ import Home from './pages/Home';
 import Projects from './pages/Projects.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact';
+import Error404 from './pages/404';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
+    <Route path='/' element={<Layout />} errorElement={<Error404 />}>
       <Route element={<Home />} index />
       <Route element={<Projects />} path='projects' />
       <Route element={<About />} path='about' />
